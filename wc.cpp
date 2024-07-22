7f6c8f22eefc95cce783b0e08e13d69f4785754f
Tejesh Kumar S <zenc0derr>
2024-07-22 21:52:17 +0530

    Added functionality to count lines

--- wc.cpp
+++ wc.cpp
@@ -3,6 +3,15 @@
 
 using namespace std;
 
+long long countLines(ifstream &file){
+    long long lineCount = 0;
+    string line;
+    while(getline(file, line)){
+        lineCount++;
+    }
+
+    return lineCount;
+}
 
 int main(int argc, char const *argv[])
 {
@@ -11,21 +20,21 @@ int main(int argc, char const *argv[])
         cout<<"Please Mention a file"<<endl;
         return 0;
     }
-
+    int choice;
     for(int i=0;i<argc;i++){
-        int choice;
         if(argv[i][0] == '-'){
-            if(argv[i][2] == 'c'){
+            if(argv[i][1] == 'c'){
                 choice = 0;
             }
-            else if(argv[i][2] == 'l'){
+            else if(argv[i][1] == 'l'){
                 choice = 1;
             }
-            else if(argv[i][2] == 'w'){
+            else if(argv[i][1] == 'w'){
                 choice = 2;
             }
         }
     }
+
 
     if(argc == 2){
         cout<<"Please Mention a file"<<endl;
@@ -33,7 +42,9 @@ int main(int argc, char const *argv[])
     }else{
         ifstream file(argv[2]);
         if(file.is_open()){
-            
+            if(choice == 1){
+                cout<<countLines(file)<<" "<<argv[2]<<endl;
+            }
         }else{
             
         }
