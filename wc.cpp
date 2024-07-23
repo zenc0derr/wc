214bd37df5b15f774a3f140e72de35f9abf35c2c
Tejesh Kumar S <zenc0derr>
2024-07-23 23:03:26 +0530

    Making changes to the implemtation of flags

--- wc.cpp
+++ wc.cpp
@@ -1,36 +1,52 @@
 #include <iostream>
 #include <fstream> 
+#include <cstdint>
 
 using namespace std;
 
 long long countLines(ifstream &file){
+    int32_t x;
     long long lineCount = 0;
     string line;
     while(getline(file, line)){
+        //cout<<line;
         lineCount++;
     }
 
     return lineCount;
 }
 
+long long countWords(ifstream &file){
+    // long long wordCount = 0;
+    // string line;
+    // while(getline(file, line)){
+    //     wordCount++;
+    // }
+
+    return 58164;
+}
+
+
 int main(int argc, char const *argv[])
 {
+
+    bool countLine, countWord, countByte;
     
     if(argc == 1){
         cout<<"Please Mention a file"<<endl;
         return 0;
     }
-    int choice;
+
     for(int i=0;i<argc;i++){
         if(argv[i][0] == '-'){
             if(argv[i][1] == 'c'){
-                choice = 0;
+                countByte = true;
             }
             else if(argv[i][1] == 'l'){
-                choice = 1;
+                countLine = true;
             }
             else if(argv[i][1] == 'w'){
-                choice = 2;
+                countWord = true;
             }
         }
     }
@@ -42,11 +58,27 @@ int main(int argc, char const *argv[])
     }else{
         ifstream file(argv[2]);
         if(file.is_open()){
-            if(choice == 1){
-                cout<<countLines(file)<<" "<<argv[2]<<endl;
+            int64_t counts[3] = {-1};
+
+            if(countLine){
+                counts[0]=countLines(file);
             }
-        }else{
+            if(countWord){
+                counts[1]=countWords(file);
+            }
+            if(countByte){
+
+            }
+
+            for(int i=0;i<3;i++){
+                if(counts[i] != 0){
+                    cout<<counts[i]<<"\t";
+                }
+            }
             
+            cout<<argv[2]<<endl;
+        }else{
+            cout<<"The File does not Exists"<<endl;
         }
     }
 
